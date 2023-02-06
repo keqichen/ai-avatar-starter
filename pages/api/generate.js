c58446dc33b59cf3a1037bf41ae82874d8e5730f
Keqi <ckq009@gmail.com>
2023-02-06 21:48:10 +0000

    Add api to generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,5 +1,23 @@
 const generateAction = async (req, res) => {
-    console.log('Received request')
-  }
+    console.log('Received request');
+
+    const input = JSON.parse(req.body).input;
+
+    const response = await fetch(
+        `https://api-inference.huggingface.co/models/buildspace/ai-avatar-generator`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.HF_AUTH_KEY}`,
+            'Content-Type': 'application/json',
+          },
+          method: 'POST',
+          body: JSON.stringify({
+            inputs: input,
+          }),
+        }
+      );
+  };
+
+
   
   export default generateAction;
